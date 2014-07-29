5eaca50a918e179c69775a2cd91f6a52d2307c35
Jaken Herman <JakenHerman@outlook.com>
2014-07-29 00:51:29 -0500

    Update unitConverter.java

--- unitConverter.java
+++ unitConverter.java
@@ -1,3 +1,18 @@
+import java.util.*;
+
 public class unitConverter {
+
+  public static void main(String[] args){
+    
+    //there are 2.54 cm in 1 inch
+    Scanner scan = new Scanner(System.in);
+    System.out.println("How many inches should I convert to Centi?");
+      double userInches = scan.nextInt();
+      
+    double convert = userInches * 2.54;
+    
+    System.out.println("There are "+convert+" centimeters in "+userInches+" inches");
+    
+  }
 
 }
